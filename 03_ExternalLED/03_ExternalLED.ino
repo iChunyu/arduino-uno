5186672dd765bd3823164b457054b2f549e969c0
Chunyu <chunyu2018@foxmail.com>
2021-11-11 23:57:03 +0800

    add ExernalLED test

---
+++ 03_ExternalLED/03_ExternalLED.ino
@@ -0,0 +1,30 @@
+// Turn on external LEDs from digital output
+// XiaoCY 2021-11-11
+
+int pinBase = 7;
+int pinNum = 3;
+int turnLED = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i < pinNum; i++) {
+    pinMode(pinBase + i, OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pinBase + turnLED, HIGH);
+  for (int i = 0; i < pinNum; i++) {
+    if (i != turnLED) {
+      digitalWrite(pinBase + i, LOW);
+    }
+  }
+  
+  turnLED += 1;
+  if (turnLED >= 3) {
+    turnLED = 0;
+  }
+
+  delay(200);
+}

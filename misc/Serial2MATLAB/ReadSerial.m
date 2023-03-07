22af661b9b4935e950e7af17183eee6b11475d4c
Chunyu <chunyu2018@foxmail.com>
2023-03-07 16:45:23 +0800

    Update serial test with MATLAB.

--- misc/Serial2MATLAB/ReadSerial.m
+++ misc/Serial2MATLAB/ReadSerial.m
@@ -1,17 +1,23 @@
+% Read serial data from Arduino and plot data
+
+% XiaoCY 2023-03-07
+
+%%
 clear;clc
 
-s = serialport('/dev/ttyUSB0', 9600);
-
+% use `serialportlist` to see serial list
+s = serialport('/dev/ttyACM0', 9600);
 fs = 1/20e-3;
+
 filename = 'testdata.dat';
-Time = 10;
-Tfft = 3;
+save_time = 10;         % time length of data to be saved
+fft_time = 3;           % time length of data to perform `fft`
 
 %%
 fid = fopen(filename,'w');
 
 % for FFT
-nfft = Tfft*fs;
+nfft = fft_time*fs;
 data = nan(nfft,1);
 N1 = floor(nfft/2);
 f = fs/nfft*(1:N1-1)';
@@ -27,10 +33,13 @@ subplot(212)
 box on
 grid on
 
-flush(s);
-for k = 1:Time*fs
+while s.NumBytesAvailable > 0
+    s.flush
+end
+for k = 1:save_time*fs
     subplot(211)
-    x = eval(s.readline);
+    x = eval(s.read(18,'char'));    % use `Serial.print` in arduino
+    % x = eval(s.readline);         % use `Serial.println` in arduino
     fprintf(fid, '%s    %+16.13f\n',datestr(now,'yyyy-mm-dd HH:MM:ss.FFF'),x);
     addpoints(an,(k-1)/fs,x)
     drawnow
@@ -53,3 +62,4 @@ for k = 1:Time*fs
 end
 
 fclose(fid);
+delete(s);
--- misc/Serial2MATLAB/Serial2MATLAB.ino
+++ misc/Serial2MATLAB/Serial2MATLAB.ino
@@ -2,28 +2,38 @@
 const int T = 20;
 
 // Sine wave
-const double pi = 3.1415926535897932;     // http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html
-const double A = 3;                             // [V] Amplitude
-const double b = 2;                             // [V] Bias: b - A >= 0 && b + A <= Vmax
-const double f = 2;                           // [Hz] Frequency
+const double pi = 3.1415926535897932;           // http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html
+const double A = 1;                             // [V] Amplitude
+const double b = 2;                             // [V] Bias
+const double f = 0.5;                           // [Hz] Frequency
 
 const int N = 1e3 / (f*T);
 int idx = 0;
 double x[N];
-double xn;
-
 
 void setup() {
   Serial.begin(9600);
   idx = 0;
   for (int i = 0; i < N; i++) {
-    x[i] = A * sin(2 * pi / N * i);
+    x[i] = A * sin(2 * pi / N * i) + b;
   }
 }
 
 void loop() {
-  xn = random(65535) / 65535.0;
-  Serial.println(x[idx] + xn, 16);
+  /** Serial.print
+  If you use `Serial.print(num, digits)` in Arduino and specify digits, you may use `read(device, count, 'char')` in MATLAB.
+  Note that: 
+    1. `count`(MATLAB) is the sum of digits of the Arduino output, while `digits`(Arduino) is the number of decimal places;
+    2. Arduino always send data decoded by ASCII, so MATLAB should read data with `char` (or `string`) option.
+  WARNING: negative value may fail because the ASCII data length changed (an extra '-' sign).
+  **/
+  Serial.print(x[idx], 16);         // MATLAB: s.read(18,'char')
+
+  /** Serial.println
+  If you use `Serial.println(num, digits)` in Arduino and specify digits, you may use `readline` in MATLAB.
+  Since `\r\n` denotes the end of the line, and `readline`(MATLAB) deals with ASCII data, no extra process is needed.
+  **/
+  //  Serial.println(x[idx], 16);       // MATLAB: s.readline
 
   idx = idx < N - 1 ? ++idx : 0;
   delay(T);
